00e0c1860cb2f4b175807f292dbe9cda9c297630
garnett <garnett@hotmail.com>
2013-10-02 23:11:18 -0400

    added new headers

--- index.php
+++ index.php
@@ -7,12 +7,21 @@
 		</head>
 		
 			<body>
-				<h1>Garnett Murray's Bio</h1>
+					<h1>Garnett Murray's Bio</h1>
 					<p>Hi, Im Garnett Murray and this is my bio! I live in Hamden CT and attend Harvard Extension. I am a senior studying psychology and Government
 					as a Social Sciences concentrator. I took a Java I course and was hooked, so I am now taking Building Dynamic Web Applications
 					So I can use my creativity and learn to build websites!</p> 
 				
 					<h2>What I Like</h2>
 					<p>	For recreation I like tennis and running. My favorite pro sports teams are the Denver Broncos, Oklahoma Sooners as well as Harvard Crimson football and basketball.</p>
+					
+					<h2>What I do</h2>	
+					<p>I work as a paraprofessional in an elementary school in Bridgeport, CT, the kids are cool(thank God). I hope to do well and get a full understanding of front end web development
+					and some day work as a web developer.
+					</p>
+			
+					<h2>Web Editor of Choice</h2>
+					<p>As a new web development student, my first web editor was Eclipse when learning Java, but I like TextWrangler best for its simplicity.
+					</p>
 			</body>
 	</html>
