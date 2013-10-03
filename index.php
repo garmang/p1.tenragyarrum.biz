7e632e3cc9e7d530bceb54f0e161380d741b4dd9
garnett <garnett@hotmail.com>
2013-10-02 23:43:17 -0400

    whole page

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-	<html>
-	
-		<head>
-		<meta charset="utf-8"><!-- This is a stylesheet coloring all h1 and h2 elements blue and background green-->
+<html>
+	<head>
+		<meta charset="UTF-8">
 		
-		
-		<title>Garnett</title>
+		<title>Garnett Murray's Bio</title>
+		<!-- This is a stylesheet coloring all h1 and h2 elements blue and background green-->
 		<style>
 			h1, h2
 			{
@@ -16,25 +15,17 @@
 			background-color: green;
 			}
 		</style>
-
-		</head>
-		
-			<body>
-					<h1>Garnett Murray's Bio</h1>
-					<p>Hi, Im Garnett Murray and this is my bio! I live in Hamden CT and attend Harvard Extension. I am a senior studying psychology and Government
-					as a Social Sciences concentrator. I took a Java I course and was hooked, so I am now taking Building Dynamic Web Applications
-					So I can use my creativity and learn to build websites!</p> 
+	</head>
+	
+		<body>
+				<h1>Garnett Murray's Bio</h1>
+				<p>Hi, Im Garnett Murray and this is my bio! I live in Hamden CT and attend Harvard Extension. I am a senior studying psychology and Government
+				as a Social Sciences concentrator. I took a Java I course and was hooked, so I am now taking Building Dynamic Web Applications
+				So I can use my creativity and learn to build websites!</p> 
 				
-					<h2>What I Like</h2>
-					<p>	For recreation I like tennis and running. My favorite pro sports teams are the Denver Broncos, Oklahoma Sooners as well as Harvard Crimson football and basketball.</p>
-					
-					<h2>What I do</h2>	
-					<p>I work as a paraprofessional in an elementary school in Bridgeport, CT, the kids are cool(thank God). I hope to do well and get a full understanding of front end web development
-					and some day work as a web developer.
-					</p>
+				<h2>What I Like</h2>
+				<p>	For recreation I like tennis and running. My favorite pro sports teams are the Denver Broncos, Oklahoma Sooners as well as Harvard Crimson football and basketball.</p>
 			
-					<h2>Web Editor of Choice</h2>
-					<p>As a new web development student, my first web editor was Eclipse when learning Java, but I like TextWrangler best for its simplicity.
-					</p>
-			</body>
-	</html>
+				<h2>What I do</h2>
+				<p>I work as a paraprofessional in an elementary school in Bridgeport, CT, the kids are cool(thank God). I hope to do well and get a full understanding of front end web development
+				and so
