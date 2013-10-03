fdeed03c6adc8e1dc07615d60ed98ea73af214f1
garnett <garnett@hotmail.com>
2013-10-02 22:55:54 -0400

    added h1 and h2 tags tags

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 	<html>
 	
-	<head>
-		<meta charset="UTF-8">
-		<title>Hello World</title>
-		<?php
-		echo "Hello World today";
-		?>
-	</head>
-	
-	<body>
+		<head>
+		<meta charset="utf-8">
+		<title>Garnett</title>
+		</head>
 		
-		<p> new line of code is the best</p>
-	</body>
-
+			<body>
+				<h1>Garnett Murray's Bio</h1>
+					<p>Hi, Im Garnett Murray and this is my bio! I live in Hamden CT and attend Harvard Extension. I am a senior studying psychology and Government
+					as a Social Sciences concentrator. I took a Java I course and was hooked, so I am now taking Building Dynamic Web Applications
+					So I can use my creativity and learn to build websites!</p> 
+				
+					<h2>What I Like</h2>
+					<p>	For recreation I like tennis and running. My favorite pro sports teams are the Denver Broncos, Oklahoma Sooners as well as Harvard Crimson football and basketball.</p>
+			</body>
 	</html>
